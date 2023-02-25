9f603178cf7bb998720f46eb25d7f5c4d3ab1429
Christina Stetsko <christina.stetsko@waveoc.com>
2023-02-25 18:31:46 +0300

    Added first lwc

---
+++ force-app/main/default/lwc/accountsContactsTree/accountsContactsTree.js
@@ -0,0 +1,28 @@
+import { LightningElement, wire } from 'lwc';
+import { publish, MessageContext } from 'lightning/messageService';
+import MESSAGE_CHANNEL from '@salesforce/messageChannel/SummaryMessageChannel__c';
+import getTreeData from '@salesforce/apex/AccountContactsController.getTreeData';
+
+export default class AccountsContactsTree extends LightningElement {
+    treeData;
+    selectedItemId;
+    @wire(MessageContext) messageContext;
+
+    @wire(getTreeData)
+    wiredAccounts({ error, data }) {
+        if (data) {
+            this.treeData = data;
+        } else if (error) {
+            console.error(error);
+        }
+    }
+
+    handleOnselect(event) {
+        this.selectedItemId = event.detail.name;
+
+        const message = {
+            selectedItemId: this.selectedItemId
+        };
+        publish(this.messageContext, MESSAGE_CHANNEL, message);
+    }
+}
